26a3b8d39bf9d3760445c102e74c0aef4b99cbdc
DisabledMallis <mallisiscool@gmail.com>
2020-05-05 16:16:24 -0400

    Adding towers C# example

---
+++ NKHook5 Examples/AddedTowers/AddTowers.cs
@@ -0,0 +1,32 @@
+﻿using NKHook5.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddedTowers
+{
+    public class AddTowers : NKPlugin
+    {
+        //Constructor for our plugin. Stores data like its name & a description of what it does.
+        public AddTowers() : base("Added Towers", "Injects 4 new tower types into the game using NKHook5")
+        {
+        }
+        //When a plugin is loaded, NKHook5 will call this function first.
+        public override unsafe void onEnable()
+        {
+            //Removes tower locking from the game, meaning you can have all the towers at rank 1 without having to unlock them all.
+            //This may be useful if your tower is locked and you are unsure why. Can help with debugging your mod.
+            Patch.removeTowerLockPatch();
+            //Injects a new flag into the game, this can be anything but in this example its a tower
+            //The corresponding file would be DRIP.tower inside tower definitions.
+            //This would also be the tower's TypeName & FactoryName for the tower file and selection menu
+            //In the future this will be able to access files like DRIP.bloon too, so better not have name conflicts!
+            Patch.injectFlag("DRIP");
+            Patch.injectFlag("0001");
+            Patch.injectFlag("0002");
+            Patch.injectFlag("Funk");
+        }
+    }
+}
---
+++ NKHook5 Examples/AddedTowers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("AddedTowers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AddedTowers")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("8f5d13bf-2714-4cf9-ab58-d1916a08a088")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
